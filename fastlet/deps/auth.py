a9f4499a3048d47c54e24e90d1c8267492244aa7
draew6 <adosko6@gmail.com>
2025-06-11 19:52:29 +0200

    newcookio for swaggerui

--- fastlet/deps/auth.py
+++ fastlet/deps/auth.py
@@ -1,9 +1,20 @@
 from typing import Annotated
 from ..queries.auth import get_db, AuthQueries
-from fastapi import Depends
+from fastapi import Depends, Request, HTTPException
 from ..models.auth import AuthCookie
-from fastapi import Cookie
+from pydantic import BaseModel, ValidationError
+
+
+def NewCookie[T: BaseModel](model: type[T]):
+    def dependency(request: Request) -> T:
+        try:
+            data = {field: request.cookies.get(field) for field in model.model_fields()}
+            return model(**data)
+        except ValidationError as e:
+            raise HTTPException(status_code=400, detail=e.errors())
+    return dependency
+
 
 
 AuthQueries = Annotated[AuthQueries, Depends(get_db)]
-RawAuthCookies = Annotated[AuthCookie, Cookie()]
+RawAuthCookies = Annotated[AuthCookie, Depends(NewCookie(AuthCookie))]
