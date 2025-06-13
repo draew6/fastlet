7d20edff5df9dead00878959d6434111b0ff9a11
draew6 <adosko6@gmail.com>
2025-06-13 17:30:29 +0200

    create refresh token tzinfo fix

--- fastlet/auth/other.py
+++ fastlet/auth/other.py
@@ -5,5 +5,5 @@ from ..deps.auth import AuthQueries
 
 async def create_refresh_token(user_id: int, db: AuthQueries) -> str:
     refresh_token = create_token()
-    date_created = datetime.now(UTC)
+    date_created = datetime.now(UTC).replace(tzinfo=None)
     return await db.create_refresh_token(refresh_token, user_id, date_created)
