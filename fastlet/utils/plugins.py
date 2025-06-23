5f02f2a52735e86718da69844b9994748d953594
draew6 <adosko6@gmail.com>
2025-06-23 17:02:31 +0200

    allow cors middleware

--- fastlet/utils/plugins.py
+++ fastlet/utils/plugins.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from importlib import import_module
 import pkgutil
+import re
+
+from pydantic import ValidationError
+
+from ..utils.settings import get_settings
 
 
 def autoload(app: FastAPI, package_name: str):
@@ -13,3 +19,17 @@ def autoload(app: FastAPI, package_name: str):
             app.include_router(module.router)
         if is_pkg:
             autoload(app, module_name)
+
+def allow_cors(app: FastAPI):
+    try:
+        settings = get_settings("bff")
+    except ValidationError:
+        settings = get_settings("auth")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:8001"],
+        allow_credentials=True,
+        allow_origin_regex=rf"^https?://(?:[a-z0-9-]+\.)*{re.escape(settings.root_domain)}(?::\d+)?$",
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
