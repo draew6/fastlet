18909132323a49f97b9cbb40dc5ac363b459062c
draew6 <adosko6@gmail.com>
2025-06-28 09:08:07 +0200

    System

--- fastlet/deps/user.py
+++ fastlet/deps/user.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 from fastapi import Depends
 from ..auth.utils.token import JWTPayload
-from ..auth.authorization import authorize, authorize_admin
+from ..auth.authorization import authorize, authorize_admin, authorize_system
 
 User = Annotated[JWTPayload, Depends(authorize)]
 Admin = Annotated[JWTPayload, Depends(authorize_admin)]
+System = Annotated[JWTPayload, Depends(authorize_system)]
